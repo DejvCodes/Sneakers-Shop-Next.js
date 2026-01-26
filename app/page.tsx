62f1d3025ce46c511c4257a512666b0f3946363e
David Kalmus <dejvios@seznam.cz>
2026-01-26 19:14:12 +0100

    feat: implement product search functionality and update product listing

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,21 @@
 'use client';
+import {Sneaker} from '@/types';
 import {LuSearch} from 'react-icons/lu';
+import {useEffect, useState} from 'react';
 import {sneakers} from '@/constants/sneakers';
 import ProductCard from '@/components/ProductCard';
 
 const Products = () => {
+  const [searchingProduct, setSearchingProduct] = useState('');
+  const [filteredProducts, setFilteredProducts] = useState<Sneaker[]>(sneakers);
+
+  useEffect(() => {
+    const productsAfterFilter = sneakers.filter((oneSneaker) => {
+      return oneSneaker.fullName.toLowerCase().includes(searchingProduct.toLowerCase());
+    })
+    setFilteredProducts(productsAfterFilter);
+  }, [searchingProduct]);
+
   return <section 
     className='w-full max-w-7xl flex items-center justify-center my-20 mx-auto px-4'
   >
@@ -11,6 +23,9 @@ const Products = () => {
       <div className='w-125 max-w-[calc(100vw-24px)] text-xl border border-black/15 flex items-center mb-6 md:mb-8 p-2.5'>
         <input
           type='text' 
+          placeholder='Vyhledat produkt ...'
+          value={searchingProduct}
+          onChange={(e) => setSearchingProduct(e.target.value)}
           className='w-full text-sm outline-none'
         />
         <LuSearch />
@@ -22,7 +37,7 @@ const Products = () => {
         max-[550px]:gap-1.75 max-[550px]:mx-0
         max-[370px]:grid-cols-1
       '>
-        {sneakers.map((oneSneaker) => {
+        {filteredProducts.map((oneSneaker) => {
           return <ProductCard key={oneSneaker.id} {...oneSneaker} />
         })}
       </div>
