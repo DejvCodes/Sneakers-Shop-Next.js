5b86ccd46752107fe7dcac275a86bce044fa60d1
David Kalmus <dejvios@seznam.cz>
2026-01-26 17:52:36 +0100

    types

---
+++ types/index.ts
@@ -0,0 +1,38 @@
+export interface NavLink {
+  text: string;
+  path: string;
+}
+
+export interface Sneaker {
+  id: number;
+  brand: string;
+  fullName: string;
+  name: string;
+  type: string;
+  price: number;
+  image: string;
+  images: string[];
+  season: string[];
+  info: string;
+  productCode: string;
+  styleColor: string;
+  slug: string;
+}
+
+export interface ProductCard {
+  id: number;
+  fullName: string;
+  price: number;
+  image: string;
+  slug: string;
+}
+
+export interface ShoppingBagItem {
+  productId: string;
+  quantity: number;
+}
+
+export interface ShoppingBagState {
+  items: ShoppingBagItem[];
+  notification: boolean;
+}
